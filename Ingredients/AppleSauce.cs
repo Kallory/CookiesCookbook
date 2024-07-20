1942037e42a0a2b192a4fcde8f1e96ebe1be7a4a
Kallory <kory232323@gmail.com>
2024-07-20 10:21:57 -0500

    adding TimeToLetSit() method to IBakingMoisture interface

--- Ingredients/AppleSauce.cs
+++ Ingredients/AppleSauce.cs
@@ -8,5 +8,12 @@ namespace Cookies_Cookbook.Ingredients {
     internal class AppleSauce : Ingredient, IBakingMoisture {
         public AppleSauce(int id, string name, string instruction) : base(id, name, instruction) {
         }
+
+        public DateTime dateTime { get => dateTime; }
+
+        public DateTime TimeToLetSit(int minutes) {
+            this.dateTime.AddMinutes(minutes);
+            return dateTime;
+        }
     }
 }
--- Ingredients/IBakingMoisture.cs
+++ Ingredients/IBakingMoisture.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Cookies_Cookbook.Ingredients {
-    public interface IBakingMoisture {
+    public interface IBakingMoisture { 
+
+        DateTime dateTime { get;}
+
+        DateTime TimeToLetSit(int minutes);
     }
 }
